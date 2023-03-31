3a429bbebe333abe318af982340d34a2cfc4e6f9
firoj10 <mdfirojhasan.info@gmail.com>
2023-03-31 11:40:57 +0600

    how to useState works Question Added

--- src/components/QAnswer/QAnswer.jsx
+++ src/components/QAnswer/QAnswer.jsx
@@ -8,6 +8,10 @@ const QAnswer = () => {
                 <p>The state is an updatable structure that is used to contain data or information about the component and can change over time. The change in state can happen as a response to user action or system event.</p>
             <p>Props are read-only components. It is an object which stores the value of attributes of a tag and work similar to the HTML attributes. It allows passing data from one component to other components</p>
             </div>
+            <div>
+                <h2>How does useState work?</h2>
+                <p>useState is React Hook that allows you to add state to a functional component. It returns an array with two values: the current state and a function to update it</p>
+            </div>
            
         </div>
     );
